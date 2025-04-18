f091edcd5f03082f0621ded6e7bddeca74787bcd
kalio007 <kalioprince1@gmail.com>
2025-04-18 23:10:28 +0100

    added some comments to help with setup

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,8 +1,12 @@
 variable "key_path" {
-  description = "Path to the private key file"
+  description = "Path to SSH private key, without extension"
   type        = string
+  default     = "~/.ssh/cosmos"
 }
 variable "aws_region" {
   description = "AWS region to deploy resources"
   default     = "us-east-1"
 }
+
+# to create your ssh key
+# ssh-keygen -t rsa -b 4096 -C "yeahthatsmyemail@gmail.com" -f ~/.ssh/cosmos
